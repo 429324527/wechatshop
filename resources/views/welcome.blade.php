1b7796a6b96acc946c6d0c3eb369b2c9e6583158
429324527 <429324527@qq.com>
2020-03-25 12:02:55 +0800

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>欢迎来到微信shop</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    welcome wechatshop    
                 </div>
 
                 <div class="links">
